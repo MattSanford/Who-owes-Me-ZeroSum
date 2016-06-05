c356723acb7d87ebbdf887ff4174fbd98a77f1d3
Matt Sanford <ma11sanford@gmail.com>
2016-06-05 16:16:18 -0400

    Fixed friends related functions
    Fixed some scoping issues as well as adding friends to the
    $scope.firendsList variable

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,8 +5,8 @@
  */
 var app = angular.module('app.Wom', ['firebase']);
 var ref = new Firebase('https://who-owes-me.firebaseio.com/'); //Where data is stored
-var usersRef = new Firebase('https://who-owes-me.firebaseio.com/users'); //Where user data is stored	
-app.controller('SampleCtrl', ['$scope', '$firebaseObject', '$firebaseArray','$firebaseAuth', function($scope, $firebaseObject, $firebaseArray, $firebaseAuth) {
+var usersRef = new Firebase('https://who-owes-me.firebaseio.com/users'); //Where user data is stored    
+app.controller('SampleCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$firebaseAuth', function($scope, $firebaseObject, $firebaseArray, $firebaseAuth) {
     $scope.authData = {};
     $scope.error = {};
     $scope.friendsList = {};
@@ -27,11 +27,10 @@ app.controller('SampleCtrl', ['$scope', '$firebaseObject', '$firebaseArray','$fi
             console.log("User is logged out");
         }
     });
-	
-    
     $scope.login = function() {
             //Use google to authenticate, allow access to the user's email 
-            auth.$authWithOAuthPopup("google", {
+            var authref = $firebaseAuth(ref);
+            authref.$authWithOAuthPopup("google", {
                 scope: "email"
             }).then(function(authData) {
                 //check if account is made
@@ -75,88 +74,68 @@ app.controller('SampleCtrl', ['$scope', '$firebaseObject', '$firebaseArray','$fi
             });
         }
     }
-    
-}]); 
-
-//search for friends
-
-$scope.search = {
-	email: ''
-};
-//result of search is saved here
-$scope.newFriend = {
-	name: '',
-	email: '',
-	pic: '',
-	uid: '',
-	google_id: ''
-}
-
-$scope.searchUser = function() {
-	if ($scope.search.email) {
-		usersRef.orderByChild('email')
-		.startAt($scope.search.email)
-		.endAt($scope.search.email)
-		.once( 'value', function(dataSnapshot) {
-  			var foundUser = dataSnapshot.val();
-  			var foundUserUid = Object.keys(foundUser);
-  			foundUser = foundUser[foundUserUid];
-  			$scope.$apply( $scope.newFriend = {
-	  			name: foundUser.name,
-	  			email: foundUser.email,
-	  			pic: foundUser.pic,
-	  			uid: foundUserUid[0],
-	  			google_id: foundUser.google_id
-  			});
-  		console.log($scope.newFriend);
-}, function(err) {
-	if (err) {
-		console.log(err);
-		$scope.error.message = err;
-	}
-});	
-	} else {
-		$scope.error.message = "Please Enter a valid email";
-	}
-}
-
-$scope.addFriend = function() {
-	ref.child('users')
-	.child($scope.authData.uid)
-	.child('friends')
-	.child($scope.newFriend.uid)
-	.set($scope.newFriend);
-	$scope.newFriend = {};
-}
-
-$scope.listFriends= function (){
-	var friendsRef = usersRef.child($scope.authData.uid).child('friends/');
-	var friendsList;
-friendsRef.once( 'value', function(dataSnapshot) {
-		friendsList = dataSnapshot.val();
-		console.log(friendsList);	
-		var objectIndicies = Object.keys(friendsList); //store $id's in an aray.
-		console.log(objectIndicies);
-  		for (var i = 0; i < Object.keys(friendsList).length; i++) {
-				$scope.friendsList = { };
-				var $id = friendsList[objectIndicies[i]].google_id;
-				var name = friendsList[objectIndicies[i]].name;
-				var email = friendsList[objectIndicies[i]].email;
-				console.log(friendsList[objectIndicies[i]]);
-				jsonObj[data[i].id] = data[i].name;
-
-				$scope.friendsList = {
-
-						id:  $id,
-						name: name,
-						email: email
-				};
-	}
-					console.log($scope.friendsList[0]);	
-
- });
-
-
-
-};
-
+    //search for friends
+    $scope.search = {
+        email: ''
+    };
+    //result of search is saved here
+    $scope.newFriend = {
+        name: '',
+        email: '',
+        pic: '',
+        uid: '',
+        google_id: ''
+    }
+    $scope.searchUser = function() {
+        if ($scope.search.email) {
+            usersRef.orderByChild('email').startAt($scope.search.email).endAt($scope.search.email).once('value', function(dataSnapshot) {
+                var foundUser = dataSnapshot.val();
+                var foundUserUid = Object.keys(foundUser);
+                foundUser = foundUser[foundUserUid];
+                $scope.$apply($scope.newFriend = {
+                    name: foundUser.name,
+                    email: foundUser.email,
+                    pic: foundUser.pic,
+                    uid: foundUserUid[0],
+                    google_id: foundUser.google_id
+                });
+                console.log($scope.newFriend);
+            }, function(err) {
+                if (err) {
+                    console.log(err);
+                    $scope.error.message = err;
+                }
+            });
+        } else {
+            $scope.error.message = "Please Enter a valid email";
+        }
+    }
+    $scope.addFriend = function() {
+        ref.child('users').child($scope.authData.uid).child('friends').child($scope.newFriend.uid).set($scope.newFriend);
+        $scope.newFriend = {};
+    }
+    $scope.listFriends = function() {
+        var friendsRef = usersRef.child($scope.authData.uid).child('friends/');
+        var data;
+        friendsRef.once('value', function(dataSnapshot) {
+            data = dataSnapshot.val();
+            console.log("Data returned from firebase: ");
+            console.dir(data)
+            var objectIndicies = Object.keys(data); //store $id's in an aray.
+            $scope.friendsList = {};
+            console.log("Number of friends: " + Object.keys(data).length);            
+            for (var i = 0; i < Object.keys(data).length; i++) {
+                var $id = data[objectIndicies[i]].google_id;
+                var name = data[objectIndicies[i]].name;
+                var email = data[objectIndicies[i]].email;
+                console.log("Data to add to $scope.friendsList, " + "id: " + $id + ", " + "name: " + name + ", " + "email: " + email);
+                $scope.friendsList[name]= {
+                        id: $id,
+                        name: name,
+                        email: email
+                };
+            }
+            console.log($scope.friendsList);
+        });
+    }
+}]);
